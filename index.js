de22c2d389988f57f6941983800e80325a17651c
Favour Ifurukpe <tinafavour2018@gmail.com>
2024-07-08 11:03:50 +0100

    class

--- index.js
+++ index.js
@@ -6,30 +6,40 @@ const PORT = 3000;
 app.get("/", (req, res) => {
   res.send("hello world");
 });
-app.get("/users", (req, res) => {
+app.get("/", (req, res) => {
   res.send("hello Favour");
 });
-// app.get("/users/deji", (req, res) => {
-//   res.send("hello deji");
+app.get("/ab?cd", (req, res) => {
+  res.send("path for ab?cd");
+});
+app.get("/ab+c+d", (req, res) => {
+  res.send("path for double b and c");
+});
+app.get("/ab*cd", (req, res) => {
+  res.send("tell me my name ");
+});
+// app.get(/a/, (req, res) => {
+//   res.send("path for /a/ ");
 // });
-app.get("/users/:name", (req, res) => {
-    if(req.params.name.length < 5)res.status(200).send(`<h1>404 not Found</h1>`)
-    
-     
-        res.status(200)
-    
-   
-    res.set('Content-Type','text/html')
+app.get(/.*able$/, (req, res) => {
+  res.send("any path that has able in it");
+});
+
+app.get("/users/:id?", (req, res) => {
+  if (req.params.id){
+  res.status(200)
+  res.status(200);
+  res.set("Content-Type", "text/html");
   res.send(`
       <html>
     <body>
-   
-    <h1 style="color:red">hello ${req.params.name}</h1>
+    <h1 style="color:red">hello ${req.params.id}</h1>
      </body>
      </html>
 
-    `);
- 
+    `);}else{
+      res.send("all users")
+    }
 });
 app.listen(PORT, () => {
   console.log(`server is running on http://localhost:${PORT}`);
