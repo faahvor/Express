2d3c4c8915ccb9793993182d2af1658439a03ca6
Favour Ifurukpe <tinafavour2018@gmail.com>
2024-07-08 11:56:25 +0100

    class3

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import express, { json } from "express";
-import data from "./data/data.json" assert {type:"json"}
+import data from "./data/data.json" assert { type: "json" };
 
 const app = express();
 
@@ -25,28 +25,33 @@ app.get("/ab*cd", (req, res) => {
 app.get(/.*able$/, (req, res) => {
   res.send("any path that has able in it");
 });
-app.get("/fruits/:fruitName?",(req,res)=>{
-const {fruitName} = req.params
-if(fruitName){
-  
-}
-})
+app.get("/fruits/:fruitName?", (req, res) => {
+  const { fruitName } = req.params;
+  if (fruitName) {
+    const fruit = data.find((f) => f.name === fruitName);
+    if (!fruit) {
+      res.status(404).send("fruit not found");
+    }
+    res.send(fruit);
+  }
+});
 
 app.get("/users/:id?", (req, res) => {
-  if (req.params.id){
-  res.status(200)
-  res.status(200);
-  res.set("Content-Type", "text/html");
-  res.send(`
+  if (req.params.id) {
+    res.status(200);
+    res.status(200);
+    res.set("Content-Type", "text/html");
+    res.send(`
       <html>
     <body>
     <h1 style="color:red">hello ${req.params.id}</h1>
      </body>
      </html>
 
-    `);}else{
-      res.send("all users")
-    }
+    `);
+  } else {
+    res.send("all users");
+  }
 });
 app.listen(PORT, () => {
   console.log(`server is running on http://localhost:${PORT}`);
